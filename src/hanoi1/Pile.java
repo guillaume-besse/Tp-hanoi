c3b8bf171ae129abcde3d83e3ad2ff99422f50ac
guillaume-besse <38402818+guillaume-besse@users.noreply.github.com>
2020-10-19 15:51:46 +0200

    test

--- src/hanoi1/Pile.java
+++ src/hanoi1/Pile.java
@@ -6,7 +6,7 @@ public class Pile<T> {
 
     // suppose 'capa' strictement positif !
     public Pile(int capa) {
-        throw new UnsupportedOperationException();
+        capacite=capa;
     }
 
     // Doit lever l'exception si la pile est pleine
